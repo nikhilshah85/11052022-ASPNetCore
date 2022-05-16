1dee83b81fb7e1cb88236bd04c6f0f1a931660ca
nikhilshah85 <nikhilshah.net@hotmail.com>
2022-05-16 10:46:15 +0000

    MVC Core with EF and SQL Server On Azure

---
+++ MVC-EF-SQL-Azure/MVC-EF-SQL-Azure/Models/EF/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVC_EF_SQL_Azure.Models.EF
+{
+    public partial class Product
+    {
+        public int PId { get; set; }
+        public string? PName { get; set; }
+        public string? PCategory { get; set; }
+        public int? PPrice { get; set; }
+        public bool? PIsInStock { get; set; }
+    }
+}
